83b4038a57d7ac41d2e8804211a932ec6eb323df
skyoo2003 <skyoo2003@gmail.com>
2019-07-04 00:15:21 +0900

    Lint 오류 수정

--- src/App.vue
+++ src/App.vue
@@ -22,14 +22,8 @@
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue';
-import Login from './views/Login.vue';
-
 export default {
   name: 'App',
-  components: {
-    Login,
-  },
   data() {
     return {
       //
