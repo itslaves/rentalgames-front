a56d37f26b31a52cc4fe67f6b15068bab2094bca
skyoo2003 <skyoo2003@gmail.com>
2019-08-02 23:25:34 +0900

    컴포넌트 Vuetify 호환성 교정 (https://github.com/itslaves/rentalgames-front/issues/25)

---
+++ tests/unit/components/ImageFilePicker.spec.js
@@ -0,0 +1,22 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import { expect } from 'chai';
+import Vuetify from 'vuetify';
+import ImageFilePicker from '@/components/ImageFilePicker.vue';
+
+const localVue = createLocalVue();
+localVue.use(Vuetify);
+
+describe('components/ImageFilePicker', () => {
+  let vuetify;
+  beforeEach(() => {
+    vuetify = new Vuetify();
+  });
+  it('assertion: default props', () => {
+    const wrapper = shallowMount(ImageFilePicker, {
+      localVue,
+      vuetify,
+    });
+    expect(wrapper.vm.$data.imageName).to.eq('');
+    expect(wrapper.vm.$data.imageContents).to.eq('');
+  });
+});
