79b5a6190f6e332aa8c4e314fc3ed033090a154d
skyoo2003 <skyoo2003@gmail.com>
2019-08-18 20:13:26 +0900

    iconfont 변경 롤백 (https://github.com/itslaves/rentalgames-front/issues/25)

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,12 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
 import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
   icons: {
-    iconfont: 'mdi',
+    iconfont: 'md',
   },
 });
